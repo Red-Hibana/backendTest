643050d5186ae5d5a30cbcb96fa7174312fe92e7
Da-Whit18 <40591389+Red-Hibana@users.noreply.github.com>
2024-04-09 15:12:58 -0400

    allow all ip

--- src/app.js
+++ src/app.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server on port 3000
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on http://0.0.0.0:3000');
 });
